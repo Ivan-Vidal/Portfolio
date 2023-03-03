32361ad5350464be3d8cc26692c865da099b0517
Ivan-Vidal <ivanvalmeida.123@gmail.com>
2023-03-03 18:20:15 -0300

    Ajustes de layout

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Observable } from 'rxjs';
 import { MainService } from './../../services/main.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -15,10 +15,30 @@ export class HomeComponent implements OnInit {
   isMobile: any;
 
   constructor(private MainService: MainService) { }
-
+  windowScrolled = false
   ngOnInit(): void {
-
+    this.onScroll()
     this.isMobile = this.MainService.isMobile$.subscribe(v => this.MainService.setIsMobile(v))
   }
+
+  onScroll() {
+    let backBlock = false
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+      backBlock = true
+     console.log(backBlock)
+    } else {
+      console.log('errou')
+    }
+  }
+
+  @HostListener ("window: scroll", []) 
+onWindowScroll () { 
+    if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 10 ){
+        this.windowScrolled = true; 
+    }    else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop <10) { 
+        this.windowScrolled = false; 
+    } 
+} 
+
 
 }
