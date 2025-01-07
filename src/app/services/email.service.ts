148b7e53d8fc709b3f221abd6969e52cf182bcab
Ivan  Almeida <ivanalmeida@MacBook-Air-de-Ivan.local>
2025-01-06 23:23:52 -0300

    ajuste url back

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
 
-  private apiUrl = 'http://localhost:3000/send-email'; // URL do backend
+  private apiUrl = environment.apiUrl; // URL do backend
 
   constructor(private http: HttpClient) {}
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
+const API_URL = 'https://portifolio-back-mzqh.onrender.com/send-email'
+
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: API_URL
 };
