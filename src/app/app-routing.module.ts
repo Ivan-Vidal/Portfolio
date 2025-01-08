e28e11a5ed19a70838878b5ab040a518a06094d7
Ivan-Vidal <ivanvalmeida.123@gmail.com>
2025-01-08 12:18:22 -0300

    ajustes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { ErrorviewComponent } from './views/errorview/errorview.component';
 import { HomeComponent } from './views/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './views/about/about.component';
-import { ContactComponent } from './views/contact/contact.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { HttpClientModule } from '@angular/common/http';
-// import { FormsModule, NgModel } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import {MatSidenavModule} from '@angular/material/sidenav';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { ErrorviewComponent } from './views/errorview/errorview.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
-import { AboutComponent } from './views/about/about.component';
 import { CardProjectComponent } from './components/card-project/card-project.component';
-import { ContactComponent } from './views/contact/contact.component';
 import { TechnologiesComponent } from './views/technologies/technologies.component';
-import { FormationComponent } from './views/formation/formation.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -26,11 +20,8 @@ import { HttpClientModule } from '@angular/common/http';
     ErrorviewComponent,
     HeaderComponent,
     ButtonComponent,
-    AboutComponent,
     CardProjectComponent,
-    ContactComponent,
     TechnologiesComponent,
-    FormationComponent
   ],
   imports: [
     BrowserModule,
