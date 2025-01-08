0239e6d0ec6a6d4cb48e78c4e237df4e6ec93aec
Ivan-Vidal <ivanvalmeida.123@gmail.com>
2025-01-08 11:06:52 -0300

    ajustes tela de erro e atualizacao ano copy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,6 @@ import { ContactComponent } from './views/contact/contact.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: '',   redirectTo: 'home', pathMatch: 'full' },
-  { path: 'about', component: AboutComponent},
-  { path: 'contact', component: ContactComponent},
   { path: '**', component: ErrorviewComponent}
 ];
 
