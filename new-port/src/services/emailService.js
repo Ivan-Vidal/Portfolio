709373d68a0b893e8a464c8bcb63f3df50e6086a
Ivan  Almeida <ivanalmeida@MacBook-Air-de-Ivan.local>
2025-09-04 00:54:20 -0300

    ajustes URL chamada back api com .env

--- new-port/src/services/emailService.js
+++ new-port/src/services/emailService.js
@@ -1,7 +1,10 @@
 // src/services/emailService.js
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
 export async function sendEmail(data) {
+  if (!API_URL) throw new Error("A URL da API não está definida");
   try {
-    const response = await fetch("https://portifolio-back-mzqh.onrender.com/send-email", {
+    const response = await fetch(API_URL, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
